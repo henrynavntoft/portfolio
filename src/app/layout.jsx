56f20876d9b39407b2c6a95fc88a0090f2624d02
Henry Navntoft <henrylnavntoft@gmail.com>
2024-01-22 14:06:21 +0100

    add favicon again

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,14 +3,12 @@ import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 export const metadata = {
   title: "Henry Navntoft | Portfolio",
   description:
     "Henry Navntoft - Copenhagen based Web Developer student, aspiring to build accessible and beautiful digital solutions.",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 const inter = Inter({
@@ -21,6 +19,9 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="text-foreground bg-background">
         <Providers>
           <Header />
