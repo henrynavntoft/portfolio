2fb46bcf25d70ba0521d140d3ccf79ff1d463a1f
Henry Navntoft <henrylnavntoft@gmail.com>
2024-09-10 15:07:40 +0200

    "..."

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -7,27 +7,33 @@ import {Button} from "@nextui-org/react";
 
 export default function ProjectsSection() {
   const projects = [
-    {
-      name: "Human Film",
-      description: "A film production company based in Denmark.",
-      link: "https://www.humanfilm.dk",
-      screenshot: "https://storage.screenshotapi.net/www_humanfilm_dk__5f3e221fdb66.png",
-    }
+   {
+  name: "Human Film",
+  description: "A film production company based in Denmark. Built with Next.js and Tailwind CSS.",
+  link: "https://www.humanfilm.dk",
+  screenshot: "/human.png",
+},
+{
+  name: "Rose Holm",
+  description: "An industrial fastener solutions provider based in Denmark. Built with WordPress and Elementor.",
+  link: "https://www.roseholm.dk",
+  screenshot: "/rose.png",
+}
   ];
 
   return (
-    <article className="grid grid-cols-2 gap-4">
+    <article className="grid sm:grid-cols-2 grid-cols-1 gap-8">
       {projects.map((project, index) => (
         <div key={index} className="py-4">
           <Image
             width={1000}
-            height={1000}
+            height={500}
             src={project.screenshot}
             alt={`${project.name} screenshot`}
-            className="mb-4 w-full h-48 object-cover"
+            className="mb-4 object-cover"
           />
           <h4 className="text-xl font-bold">{project.name}</h4>
-          <p className="mb-4 font-light">{project.description}</p>
+          <p className="mb-4 font-light max-w-[80%]">{project.description}</p>
           <Button> 
           <Link href={project.link} target="_blank">
             View Project
