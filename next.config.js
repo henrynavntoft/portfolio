fbb63a8d94a946996b4216eb12f679fe5804f80d
Henry Navntoft <henrylnavntoft@gmail.com>
2024-09-10 14:50:10 +0200

    "..."

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['storage.screenshotapi.net'],
+    },
+}
 
 module.exports = nextConfig
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,33 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ProjectsSection() {
   const projects = [
     {
-      name: "Project One",
-      description: "Description of project one.",
+      name: "Human Film",
+      description: "A film production company based in Denmark.",
       link: "/projects/project-one",
-    },
-    {
-      name: "Project Two",
-      description: "Description of project two.",
-      link: "/projects/project-two",
-    },
-    {
-      name: "Project Three",
-      description: "Description of project three.",
-      link: "/projects/project-three",
-    },
-    {
-      name: "Project Four",
-      description: "Description of project four.",
-      link: "/projects/project-four",
-    },
+      screenshot: "https://storage.screenshotapi.net/www_humanfilm_dk__5f3e221fdb66.png",
+    }
   ];
 
   return (
     <article className="grid grid-cols-2 gap-4">
       {projects.map((project, index) => (
         <div key={index} className="border border-black my-4 p-4 rounded-lg">
+          <Image
+            width={1000}
+            height={1000}
+            src={project.screenshot}
+            alt={`${project.name} screenshot`}
+            className="mb-4 w-full h-48 object-cover rounded-lg"
+          />
           <h4 className="text-xl font-bold mb-2">{project.name}</h4>
           <p className="mb-4">{project.description}</p>
           <Link href={project.link}>
